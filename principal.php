b8d6750f1dec91810cb47ad41c64e62740e57dac
SaidMauricio <44823175+SaidMauricio@users.noreply.github.com>
2018-11-07 20:29:28 -0500

    Add files via upload

---
+++ principal.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        
+        Primer ejercicio:
+        <form method="post" action="recibir3.php">
+        Digite algo:
+        <input type="text" name="cadena">
+        <input type="submit" name="enviar1">
+        <br>
+        </form>
+            <hr>
+            Segundo ejercicio: 
+            <br>
+            <hr>
+            <form method="post" action="recibir.php">
+            Digite el primer numero:
+            <input type="text" name="a">
+            Digite el segundo numero:
+            <input type="text" name="b">
+            <input type="submit" name="enviar2">
+            </form> 
+            <br>
+                <hr>
+                Tercer ejercicio:
+                <br>
+                <hr>
+                <form method="post" action="recibir2.php">
+                Digite el primer dato:
+                <input type="text" name="dato">
+                Digite el segundo dato:
+                <input type="text" name="dato1">
+                <input type="submit" name="enviar3">
+                </form>
+        
+        
+    </body>
+</html>
